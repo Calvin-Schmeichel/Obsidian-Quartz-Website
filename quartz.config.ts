5f93ccb181f9f56762b4f700a599689209d7a6cd
Calvin-Schmeichel <93797574+Calvin-Schmeichel@users.noreply.github.com>
2026-01-02 08:30:21 +0000

    Quartz sync: Jan 2, 2026, 8:30 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -69,7 +69,7 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({ markdownLinkResolution: "relative" }), // shortest https://quartz.jzhao.xyz/plugins/CrawlLinks
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
